31327849197aef81d6e7224eb46c1c0d9bc966f6
Adam Johnson <me@adamj.eu>
2020-10-02 09:52:13 +0100

    Add support for a DJANGO_READ_ONLY setting (#1)

--- tests/test_django_read_only.py
+++ tests/test_django_read_only.py
@@ -1,34 +1,72 @@
 import os
+from functools import partial
 from unittest import mock
 
 import pytest
 from django.contrib.sites.models import Site
 from django.db import transaction
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 import django_read_only
 
+set_env_vars = partial(mock.patch.dict, os.environ)
+
 
 class DjangoReadOnlyTests(TestCase):
-    def test_ready_default_false(self):
+    def test_set_read_only_default_false(self):
         """
-        Check that, in the absence of a value for DJANGO_READ_ONLY,
-        AppConfig.ready() defaults read only mode to OFF.
+        Check that, in the absence of a value for the setting and environment
+        variable, AppConfig.ready() defaults read only mode to OFF.
         """
-        with mock.patch.dict(os.environ, {"DJANGO_READ_ONLY": ""}):
-            django_read_only.DjangoReadOnlyAppConfig("name", django_read_only).ready()
+        with set_env_vars(DJANGO_READ_ONLY=""):
+            django_read_only.set_read_only()
 
-        assert not django_read_only.read_only
+            assert not django_read_only.read_only
 
-    def test_ready_env_var_set(self):
+    def test_set_read_only_setting_on(self):
         """
-        Check that if DJANGO_READ_ONLY is set to anything, the
-        AppConfig.ready() sets read only mode ON.
+        Check that if the setting is True, AppConfig.ready() defaults read only
+        mode to ON.
         """
-        with mock.patch.dict(os.environ, {"DJANGO_READ_ONLY": "something"}):
-            django_read_only.DjangoReadOnlyAppConfig("name", django_read_only).ready()
+        with set_env_vars(DJANGO_READ_ONLY=""), override_settings(
+            DJANGO_READ_ONLY=True
+        ):
+            django_read_only.set_read_only()
 
-        assert django_read_only.read_only
+            assert django_read_only.read_only
+
+    def test_set_read_only_env_var_on(self):
+        """
+        Check that if the environment variable is set to anything whilst the
+        setting is undefined, AppConfig.ready() sets read only mode ON.
+        """
+        with set_env_vars(DJANGO_READ_ONLY="something"):
+            django_read_only.set_read_only()
+
+            assert django_read_only.read_only
+
+    def test_set_read_only_setting_precedence_to_env_var(self):
+        """
+        Check that if both the setting and environment variable are set,
+        the setting takes precendence.
+        """
+        with set_env_vars(DJANGO_READ_ONLY="=something"), override_settings(
+            DJANGO_READ_ONLY=False
+        ):
+            django_read_only.set_read_only()
+
+            assert not django_read_only.read_only
+
+    def test_setting_changed(self):
+        """
+        Check that if the setting changes, read_only mode updates
+        appropriately.
+        """
+        with override_settings(DJANGO_READ_ONLY=False):
+            assert not django_read_only.read_only
+
+            with override_settings(DJANGO_READ_ONLY=True):
+                assert django_read_only.read_only
 
     def test_disable_writes(self):
         django_read_only.disable_writes()
