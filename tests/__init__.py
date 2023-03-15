bf3667ad06740480ccb4c816f311007832863d6e
Adam Johnson <me@adamj.eu>
2023-03-15 11:23:01 +0000

    fix ignore comment

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,11 +8,11 @@ coverage.process_startup()
 
 
 # Patch for IPythonTests
-# Fool Django’s shell command into thinking we’re a TTY, so that it continuse
+# Fool Django’s shell command into thinking we’re a TTY, so that it continues
 # to open IPython
 if not sys.stdin.isatty():  # pragma: no cover
 
     def fake_isatty(*args, **kwargs):
         return True
 
-    sys.stdin.isatty = fake_isatty  # type: ignore [assignment]
+    sys.stdin.isatty = fake_isatty  # type: ignore [method-assign]
